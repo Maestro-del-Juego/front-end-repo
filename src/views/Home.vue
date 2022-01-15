7e093430f51b122723f6ecd0ccb7a633d4ffa27e
James Moore <james816am@gmail.com>
2022-01-14 21:32:12 -0500

    Homepage fixed, pulls icons from remote urls

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,34 @@
 <template>
   <div class="home">
-    <img class="home-image" alt="Board game stock image" src="../assets/BoardGameStock1.jpeg" />
+    <img class="home-image" alt="Board game stock image" src="../assets/catan.jpg" />
     <div class="feature-list">
       <FeatureBlock
+        featureIconSrc="https://www.svgrepo.com/show/83116/board-games-set.svg"
         featureName="Feature Name 1"
         featureDetails="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales, magna commodo gravida vestibulum, leo nisl viverra neque, id hendrerit."
       />
       <FeatureBlock
+        featureIconSrc="https://www.svgrepo.com/show/56511/board-game-blocks.svg"
         featureName="Feature Name 2"
         featureDetails="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales, magna commodo gravida vestibulum, leo nisl viverra neque, id hendrerit."
       />
       <FeatureBlock
+        featureIconSrc="https://www.svgrepo.com/show/224/chess.svg"
         featureName="Feature Name 3"
         featureDetails="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales, magna commodo gravida vestibulum, leo nisl viverra neque, id hendrerit."
       />
       <FeatureBlock
+        featureIconSrc="https://www.svgrepo.com/show/1521/backgammon.svg"
         featureName="Feature Name 4"
         featureDetails="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales, magna commodo gravida vestibulum, leo nisl viverra neque, id hendrerit."
       />
       <FeatureBlock
+        featureIconSrc="https://www.svgrepo.com/show/159394/game-board.svg"
         featureName="Feature Name 5"
         featureDetails="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales, magna commodo gravida vestibulum, leo nisl viverra neque, id hendrerit."
       />
       <FeatureBlock
+        featureIconSrc="https://www.svgrepo.com/show/39638/parchis.svg"
         featureName="Feature Name 6"
         featureDetails="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales, magna commodo gravida vestibulum, leo nisl viverra neque, id hendrerit."
       />
@@ -54,13 +60,16 @@ export default class Home extends Vue {}
   display: flex;
   flex-wrap: wrap;
   margin: 30px;
+  justify-content: center;
 }
 .feature-list * {
-  width: 25%;
-  margin-left: 5%;
+  margin-left: 3%;
+  margin-right: 3%;
+  width: 22%;
   }
 .home-image {
-  height: 450px;
-  width: 80%;
+  margin-top: 30px;
+  height: 400px;
+  width: 70%;
 }
 </style>
